a0c21364f03c74a3656432d623d8662bac44af4d
Nishimoto Shinnosuke <shinnosuke-n@pine22.naist.jp>
2016-11-03 17:59:46 +0900

    reshaped

--- added_data.py
+++ added_data.py
@@ -4,27 +4,26 @@
 import numpy as np
 
 def main(word2vec_models,data_list,knn):
-    added_list=[]
-   
-      for data_tuple in data_list:
-        glob_list=[]
+	added_list=[]
 
-        for word in data_tuple[0]:
-            try:
-                cand_word_list=word2vec_models.most_similar(word)
-            except: continue
+	for data_tuple in data_list:
+        	glob_list=[]
 
-            tempo_list=[]
+        	for word in data_tuple[0]:
+            		try:
+                		cand_word_list=word2vec_models.most_similar(word)
+            		except: continue	
+			tempo_list=[]
 
-            for i in range(knn):
-                tempo_list.append(cand_word_list[i][0])
-            glob_list.append(tempo_list)
+			for i in range(knn):
+				tempo_list.append(cand_word_list[i][0])
+			glob_list.append(tempo_list)
 
-        for s in range(knn):
-            ttmp_list=[]
+		for s in range(knn):
+			ttmp_list=[]
 
-            for list_ in glob_list:
-                ttmp_list.append(list_[s])
-            added_list.append((ttmp_list,data_tuple[1]))
+			for list_ in glob_list:
+				ttmp_list.append(list_[s])
+			added_list.append((ttmp_list,data_tuple[1]))
 
-    return (added_list)
+	return (added_list)
